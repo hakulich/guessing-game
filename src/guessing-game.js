5bcb5cb1f03ebbdbd62a55952ddc97b85af695a6
Hanna Akulich <hanna.aleinik@gmail.com>
2023-09-19 16:44:36 +0200

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.guessValue = Math.ceil((this.max - this.min) / 2 + this.min);
 
-    guess() {
+    return this.guessValue;
+  }
 
-    }
+  lower() {
+    this.max = this.guessValue;
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.guessValue;
+  }
 }
 
 module.exports = GuessingGame;
